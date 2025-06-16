76d1521bdc1bd511e2465b1631d247ba40ac2ca5
Ykuyo <ykuyo.dev@gmail.com>
2025-06-15 20:10:04 -0700

    Shortened paths. Release

--- service.ts
+++ service.ts
@@ -2,5 +2,5 @@ import { ServiceDescriptor } from '@selfage/service_descriptor';
 
 export let VIDEO_NODE_SERVICE: ServiceDescriptor = {
   name: "VideoNodeService",
-  path: "/v/n",
+  path: "/vn",
 }
